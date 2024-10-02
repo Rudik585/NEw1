7d263117126148cf5bfa70108beb7b5ac6490150
Rudik585 <rudik585@icloud.com>
2024-10-02 18:39:56 +0300

    delete line

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-
 app.set('views', path.join(__dirname, 'views'));
-
 
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-
   res.render('index', { title: 'Welcome Page', message: 'Hello, Express with EJS!' });
 });
 

b6c6c403d313b05d4fe43874ede52dca89ca5515
Rudik585 <rudik585@icloud.com>
2025-03-12 13:53:37 +0200

    menu-js

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -6,7 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const btn = document.querySelector('#language-switch');
     const language = document.querySelector('#languages-dropdown');
-    const hov = document.querySelector('#hov');
+    const avatar = document.querySelector('#avatar');
+    const menu = document.querySelector('#avatar-details');
+    const messages = document.querySelector('#messages');
+    const messagesMenu = document.querySelector('#messages-menu');
+    const notification = document.querySelector('#notification');
+    const notificationMenu = document.querySelector('#notification-menu');
+    const settings = document.querySelector('#settings');
+    const settingsMenu = document.querySelector('#settings-menu');
 
     btn.addEventListener('click', () => {
         if (language.classList.contains('active') == true) {
@@ -23,6 +30,45 @@ document.addEventListener('DOMContentLoaded', () => {
             btn.setAttribute('aria-expanded', 'false');
         } else {
             btn.setAttribute('aria-expanded', 'true');
+        }
+    });
+
+    avatar.addEventListener('click', () => {
+        if (menu.classList.contains('avatar-active') == true) {
+            menu.classList.remove('avatar-active');
+        } else {
+            menu.classList.add('avatar-active');
+        }
+        if (avatar.classList.contains('avatar-active') == true) {
+            avatar.classList.remove('avatar-active');
+        } else {
+            avatar.classList.add('avatar-active');
+        }
+        if (avatar.getAttribute('aria-expanded') == 'true') {
+            avatar.setAttribute('aria-expanded', 'false');
+        } else {
+            avatar.setAttribute('aria-expanded', 'true');
+        }
+    });
+    messages.addEventListener('click', () => {
+        if (messagesMenu.classList.contains('header-active') == true) {
+            messagesMenu.classList.remove('header-active');
+        } else {
+            messagesMenu.classList.add('header-active');
+        }
+    });
+    notification.addEventListener('click', () => {
+        if (notificationMenu.classList.contains('header-active') == true) {
+            notificationMenu.classList.remove('header-active');
+        } else {
+            notificationMenu.classList.add('header-active');
+        }
+    });
+    settings.addEventListener('click', () => {
+        if (settingsMenu.classList.contains('header-active') == true) {
+            settingsMenu.classList.remove('header-active');
+        } else {
+            settingsMenu.classList.add('header-active');
         }
     });
 });
